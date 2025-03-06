5128ae8a7df8dc39a34d969281c14ad76bca6d14
denishamenushko <150679026+denishamenushko@users.noreply.github.com>
2025-03-06 23:30:37 +0300

    Update model.cpp

--- model.cpp
+++ model.cpp
@@ -1,3 +1,63 @@
-#include "model.hpp"
+#include "Model.hpp"
 
-Model::Model() {}
+Model::Model()
+{
+    SDL_Log("[Model::Model] %p", this);
+}
+
+Model::~Model()
+{
+    SDL_Log("[Model::~Model] %p", this);
+
+}
+
+SDL_AppResult Model::init()
+{
+    if(!SDL_Init(SDL_INIT_VIDEO))
+    {
+        SDL_Log("[Model::init] Couldn't init SDL: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    if(!SDL_CreateWindowAndRenderer(
+            "Model"
+            , 480, 360
+            , 0
+            , &this->window
+            , &this->renderer
+            )
+        )
+    {
+        SDL_Log("[Model::init] Couldn't create window and renderer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult Model::onEvent(SDL_Event *event)
+{
+    switch(event->type) {
+    case SDL_EVENT_QUIT:
+        return SDL_APP_SUCCESS;
+    }
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult Model::iterate()
+{
+    this -> clearWindow();
+
+    SDL_RenderPresent(this -> renderer);
+    return SDL_APP_CONTINUE;
+}
+
+void Model::clearWindow()
+{
+    SDL_SetRenderDrawColorFloat(
+        this->renderer
+        , 0.5f, 0.5f, 0.5f
+        , SDL_ALPHA_OPAQUE_FLOAT);
+
+    SDL_RenderClear(this -> renderer);
+}
