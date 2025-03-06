2d6a9a2cc8766a74ccf5f855146a6ebe80e0101a
denishamenushko <150679026+denishamenushko@users.noreply.github.com>
2025-03-06 23:31:31 +0300

    Update model.hpp

--- model.hpp
+++ model.hpp
@@ -1,10 +1,21 @@
-#ifndef MODEL_HPP
-#define MODEL_HPP
+#pragma once
+
+#include <SDL3/SDL.h>
 
 class Model
 {
+    SDL_Window *window = nullptr;
+    SDL_Renderer *renderer = nullptr;
+
 public:
     Model();
+    ~Model();
+
+    SDL_AppResult init();
+    SDL_AppResult onEvent(SDL_Event *event);
+    SDL_AppResult iterate();
+
+protected:
+    void clearWindow();
 };
 
-#endif // MODEL_HPP
